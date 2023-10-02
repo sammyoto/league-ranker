c54227217c67e59d0c17cade244d35da729d6fec
Sam <samgfales@gmail.com>
2023-10-02 17:56:03 -0600

    added feature vector function definitions

---
+++ data_scripts/create_feature_vectors.py
@@ -0,0 +1,11 @@
+# Create a feature vector for a given player to feed to the ML model
+
+def create_feature_vector(playerID):
+    return 0
+
+def get_team_feature_vectors(teamID):
+    return 0
+
+def get_match_feature_vectors(esportsID):
+    return 0
+
